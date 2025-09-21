d5f181a6ed29e445c665f23979aacaeb85b12b3b
최소현 SK C&C <aledjdygus9300@gmail.com>
2025-09-21 17:28:22 +0900

    style: 다크모드 배경색 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100`}>
+      <body className={`${inter.className} min-h-screen bg-white dark:bg-gray-950 text-gray-900 dark:text-gray-100`}>
         <ThemeProvider>
           <div className="flex flex-col min-h-screen">
             <Navigation />
